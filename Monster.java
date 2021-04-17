cc247ba30923a5c3bd7ccaba16e1c5cee3c12934
Asa Danenhower <danenha@cf418-09.cs.wwu.edu>
2021-04-16 17:12:19 -0700

    Implement Monster Health

--- Monster.java
+++ Monster.java
@@ -3,6 +3,27 @@ import java.util.Random;
 public class Monster {
 	/* Current health of this Monster */
     private int health;
+    // private final int DENEKE_HEALTH = 55;
+    // private final int GOBLIN_HEALTH = 6;
+    // private final int ZOMBIE_HEALTH = 12;
+    // private final int ORC_HEALTH = 18;
+//idk if any of this is helpful but i hope it is! :) 
+    public Monster(int health) {
+        this.health = health;
+
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+    public int getHealth(){
+        return health;
+    }
+    public void setHealth(int health) {
+        if(this.health > 0){
+        
+        this.health = health;
+        }
+    }
 
     /* Max damage this Monster inflicts */
     private int damage;
