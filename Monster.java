9aeb13b4eef83962a2f8ea63f3f2a06bb3fbe0a2
Asa Danenhower <asadanenhower@gmail.com>
2021-04-16 17:45:28 -0700

    Implement Monster Constructor Based on Monster Type

--- Monster.java
+++ Monster.java
@@ -1,20 +1,42 @@
 import java.util.Random;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Or;
+
 public class Monster {
+
+    private final int DENEKE_HEALTH = 55;
+    private final int GOBLIN_HEALTH = 6;
+    private final int ZOMBIE_HEALTH = 12;
+    private final int ORC_HEALTH = 18;
+
 	/* Current health of this Monster */
     private int health;
-    // private final int DENEKE_HEALTH = 55;
-    // private final int GOBLIN_HEALTH = 6;
-    // private final int ZOMBIE_HEALTH = 12;
-    // private final int ORC_HEALTH = 18;
-//idk if any of this is helpful but i hope it is! :) 
-    public Monster(int health) {
-        this.health = health;
 
-        if (this.health < 0) {
-            this.health = 0;
+    /* Max damage this Monster inflicts */
+    private int damage;
+
+    /* Type of this Monster */
+    private String monsterType;
+    
+
+    public Monster(String monsterType) {
+        this.monsterType = monsterType;
+        switch (this.monsterType) {
+            case "Deneke": 
+                health = DENEKE_HEALTH;
+                break;
+            case "Goblin":
+                health = GOBLIN_HEALTH;
+                break;
+            case "Zombie":
+                health = ZOMBIE_HEALTH;
+                break;
+            case "Orc":
+                health = ORC_HEALTH;
+                break;
         }
     }
+
     public int getHealth(){
         return health;
     }
@@ -24,12 +46,6 @@ public class Monster {
         this.health = health;
         }
     }
-
-    /* Max damage this Monster inflicts */
-    private int damage;
-
-    /* Type of this Monster */
-    private String monsterType;
 
     /* Hits the targeted Player */
     public void attack(Player target) {
