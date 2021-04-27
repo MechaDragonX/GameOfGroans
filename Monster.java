e23558f12dd238498e22f54fe641a6888b39306f
Asa Danenhower <asadanenhower@gmail.com>
2021-04-27 14:57:31 -0700

    finished monster added implemented methods

--- Monster.java
+++ Monster.java
@@ -3,10 +3,14 @@ import java.util.Random;
 public class Monster {
 
     private final int DENEKE_HEALTH = 55;
+    private final int DENEKE_DAMAGE = 5; 
     private final int GOBLIN_HEALTH = 6;
+    private final int GOBLIN_DAMAGE = 10;
     private final int ZOMBIE_HEALTH = 12;
+    private final int ZOMBIE_DAMAGE = 15;
     private final int ORC_HEALTH = 18;
-
+    private final int ORC_DAMAGE = 20;
+   
 	/* Current health of this Monster */
     private int health;
     
@@ -17,21 +21,26 @@ public class Monster {
     /* Type of this Monster */
     private String monsterType;
     
+    
 
     public Monster(String monsterType) {
         this.monsterType = monsterType;
         switch (this.monsterType) {
             case "Deneke": 
                 health = DENEKE_HEALTH;
+                damage = DENEKE_DAMAGE;
                 break;
-            case "Goblin":
+            case "Goblin": 
                 health = GOBLIN_HEALTH;
+                damage = GOBLIN_DAMAGE;
                 break;
-            case "Zombie":
+            case "Zombie": 
                 health = ZOMBIE_HEALTH;
+                damage = ZOMBIE_DAMAGE;
                 break;
-            case "Orc":
+            case "Orc": 
                 health = ORC_HEALTH;
+                damage = ORC_DAMAGE;
                 break;
         }
     }
@@ -62,12 +71,20 @@ public class Monster {
 
     /* Hits the targeted Player */
     public void attack(Player target) {
-
+        Random rand = new Random();
+        
+        damage = rand.nextInt(damage) + 1;
+        target.setHealth(target.getHealth() - damage);
+             
     }
 
     /* Removes health from this Monster
     when hit by a Player */
     public void onHit(int damage) {
-
+        health -= damage;
+        if(health < 0){
+            health = 0; 
+            System.out.printf(monsterType, "has been slain!");
+        } 
     }
 }
