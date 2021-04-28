17f4d2f9b3cf9e2af325789ec87a53c4bcffd2b6
MechaDragonX <theofficalrv@gmail.com>
2021-04-27 17:52:38 -0700

    Add room constructor

--- Room.java
+++ Room.java
@@ -7,6 +7,73 @@ public class Room {
     /* Indicates whether or not this Room
     instance has been visited already */
     private boolean visited;
+    /*
+        An enum with three possible values:
+        0: Standard
+        1: Item
+        2: Monster
+    */
+    private RoomType type;
+    /*
+        An emum with three possible values:
+        0: None
+        1: Gold
+        2: Elixr
+    */
+    private ItemType item;
+    // The monster present in the room (if there is any)
+    private Monster monster;
+
+    public Room() {
+        // A seed is passed to the contructor to avoid different instances of random returning the same values
+        Random rng = new Random(48651);
+        // Roll a random number between 0 and 3 (exclusive) for the room type
+        int randomValue = rng.nextInt(3);
+        switch(randomValue) {
+            case 0:
+                type = RoomType.Standard;
+                break;
+            case 1:
+                type = RoomType.Item;
+                break;
+            case 2:
+                type = RoomType.Monster;
+                break;
+        }
+
+        if(type == RoomType.Item) {
+            // Reuse the same variable to roll a number between 0 and 3 (exclusive) for the item type
+            randomValue = rng.nextInt(3);
+            switch(randomValue) {
+                case 0:
+                    item = ItemType.None;
+                    break;
+                case 1:
+                    item = ItemType.Gold;
+                    break;
+                case 2:
+                    item = ItemType.Elixr;
+                    break;
+            }
+        } else if(type == RoomType.Monster) {
+            // Reuse the same variable to roll a number between 0 and 4 (exclusive) for the monster type
+            randomValue = rng.nextInt(4);
+            switch(randomValue) {
+                case 0:
+                    monster = new Monster("Goblin");
+                    break;
+                case 1:
+                    monster = new Monster("Zombie");
+                    break;
+                case 2:
+                    monster = new Monster("Orc");
+                    break;
+                case 3:
+                    monster = new Monster("Deneke");
+                    break;
+            }
+        }
+    }
 
 	/* Handles encounter logic when a Player
 	enters this Room. Includes combat resolution
